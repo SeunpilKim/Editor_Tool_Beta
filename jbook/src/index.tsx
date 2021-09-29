47ab365a32f2317bef959765fa0c2ad540bbf5ae
SeunpilKim <tjsvlf21@naver.com>
2021-09-29 16:12:20 +0900

    파일 타입별 onLoad 함수 분리 코드 에디터 플러그인 추가 bulma css 추가 코드 에디터에 hightlighter 플러그인 적용

--- jbook/src/index.tsx
+++ jbook/src/index.tsx
@@ -1,17 +1,20 @@
+import 'bulmaswatch/superhero/bulmaswatch.min.css'
 import { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom'
 import * as esbuild from 'esbuild-wasm'
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin'
+import CodeEditor from './components/code-editor';
+
 const App = () => {
     const ref = useRef<any>();
+    const iframe = useRef<any>();
     const [input, setInput] = useState('');
-    const [code, setCode] = useState('');
 
     const startService = async () => {
         ref.current = await esbuild.startService({
             worker: true,
-            wasmURL: '/esbuild.wasm'
+            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
         })
     }
 
@@ -23,6 +26,9 @@ const App = () => {
         if (!ref.current) {
             return;
         }
+
+        iframe.current.srcdoc = html;
+
         const result = await ref.current.build({
             entryPoints: ['index.js'],
             bundle: true,
@@ -31,16 +37,39 @@ const App = () => {
             define: { 'process.env.NODE_ENV': '"production"', global: 'window' }
         })
 
-        setCode(result.outputFiles[0].text);
+        // setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
     }
 
-    return <div>
-        <textarea value={input} onChange={(e) => setInput(e.target.value)} ></textarea>
+    const html =`
+       <html>
+        <head></head>
+        <body>
+            <div id="root"></div>
+            <script>
+                window.addEventListener('message', (event) => {
+                    try {
+                        eval(event.data)
+                    } catch (err) {
+                        const root = document.querySelector('#root');
+                        root.innerHTML = '<div style="color: red;"><h4>Runtime Error</h4>' + err + '</div>'
+                        console.error(err);
+                    }
+                }, false)
+            </script>
+        </body>
+       </html>
+    `
+
+    return (
         <div>
-            <button onClick={onClick}>Submit</button>
+            <CodeEditor initalValue={input}
+                onChange={(value) => {setInput(value)}}/>
+            <textarea value={input} onChange={(e) => setInput(e.target.value)}></textarea>
+            <div><button onClick={onClick}>Submit</button></div>
+            <iframe ref={iframe} title="code preview" sandbox="allow-scripts" srcDoc={html}/>
         </div>
-        <pre>{code}</pre>
-    </div>;
+    );
 }
 
 ReactDOM.render(
